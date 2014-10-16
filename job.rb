7c8dacce4507d869533a1a0ef9c7a90a2d03e681
Sean Holden <seanholden1@gmail.com>
2014-10-16 18:47:27 +0100

    added a validator method to clean up the sort method slightly. Handle validations on one line.

--- job.rb
+++ job.rb
@@ -1,9 +1,28 @@
 class Job
   def sort(input='')
-    return error_message unless input.kind_of? String
+    return error_message( validate(input) ) unless validate(input) == 'valid'
   end
 
-  def error_message
-    "Error: Input must be a comma separated string."
+
+  private
+
+  def validate(input)
+    if !input.kind_of? String
+      "non_string"
+    elsif input.empty?
+      "no_argument"
+    else
+      "valid"
+    end
+  end
+
+  # Method to simply return error messsages
+  def error_message(error_type)
+    case error_type
+    when 'non_string'
+      "Error: Input must be a comma separated string."
+    when 'no_argument'
+      "Note: An argument must be entered."
+    end
   end
 end
--- spec/job/job_spec.rb
+++ spec/job/job_spec.rb
@@ -2,11 +2,20 @@ require './spec/spec_helper'
 
 describe Job do
   let(:job) { Job.new }
+  let(:error_message) { "Error: Input must be a comma separated string." }
+  let(:no_argument_message) { "Note: An argument must be entered." }
 
   context "sort method" do
 
     it "returns appropriate error message if non-string argument is entered" do
-      expect( job.sort(123) ).to eq "Error: Input must be a comma separated string."
+      # Testing for integer, array and hash input
+      expect( job.sort(123) ).to eq error_message
+      expect( job.sort(['test','array']) ).to eq error_message
+      expect( job.sort({:test=>'hash'}) ).to eq error_message
+    end
+
+    it "returns appropriate message if nothing is entered as an argument" do
+      expect( job.sort ).to eq no_argument_message
     end
 
   end

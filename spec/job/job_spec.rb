873358db1d9c264a33429b8b2325f1af21b64e51
Sean Holden <seanholden1@gmail.com>
2014-10-16 20:41:25 +0100

    just moving things around to make more sense

--- spec/job/job_spec.rb
+++ spec/job/job_spec.rb
@@ -4,11 +4,11 @@ describe Job do
   let(:error_message) { "Error: Input must be a comma separated string." }
   let(:no_argument_message) { "Note: An argument must be entered." }
 
-  describe "sort method" do
 
+  describe "initializer" do
     context "non-string argument is entered" do
-
       let(:error_message) { "Error: Input must be a comma separated string." }
+
       it "integer argument returns correct error" do
         expect{ Job.new(123) }
         .to raise_error(ArgumentError,error_message )
@@ -23,19 +23,18 @@ describe Job do
         expect{ Job.new({:test=>'hash'}) }
         .to raise_error(ArgumentError, error_message)
       end
-
     end
+  end
 
+  describe "sort method" do
     context "no argument is entered" do
-
       let(:error_message) { "Error: An argument must be entered." }
+
       it "returns appropriate message if nothing is entered as an argument" do
         expect{ Job.new() }
         .to raise_error(ArgumentError, error_message)
       end
-
     end
-
   end
 
 end

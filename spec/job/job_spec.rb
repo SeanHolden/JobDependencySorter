7d5ff2bb99a4b25a1c65856feeb8f8bb35e581be
Sean Holden <seanholden1@gmail.com>
2014-10-17 19:11:13 +0100

    best restart tests again with new strategy in mind...

--- spec/job/job_spec.rb
+++ spec/job/job_spec.rb
@@ -37,34 +37,42 @@ describe Job do
 
   describe "sort method" do
     it "should sort jobs correctly (test 1)" do
+      pending
+      fail
       job = Job.new("a =>,b => c,c => f,d => a,e => b,f =>")
       expect( job.sort ).to eq %w( a f c b d e )
     end
 
     it "should sort jobs correctly (test 2)" do
+      pending
+      fail
       job = Job.new("a =>,b => c, c=>")
       expect( job.sort ).to eq %w( a c b )
     end
 
     it "should sort jobs correctly (test 3)" do
+      pending
+      fail
       job = Job.new("a=>c,b=>,c=>g,d=>e,e=>,f=>a,g=>")
       expect( job.sort ).to eq %w(g c a b e d f)
     end
 
     it "should sort jobs correctly (test 4)" do
+      pending
+      fail
       job = Job.new("a=>,b=>e,c=>f,d=>a,e=>c,f=>")
       expect( job.sort ).to eq %w( a d f c e b )
     end
 
     context "errors" do
       it "returns error when a job is trying to depend on itself" do
-        job = Job.new("a =>,b=>,c=>c")
-        expect{ job.sort }.to raise_error(StandardError, "Jobs can't depend on themselves.")
+        pending
+        fail
       end
 
       it "returns error when a job is trying to have circular dependencies" do
-        job = Job.new("a =>,b => c,c => f,d => a,e =>,f => b")
-        expect{ job.sort }.to raise_error(StandardError, "Can't have circular dependencies.")
+        pending
+        fail
       end
     end
   end
